0bacb151741951c889d0d106f3957f48d3aba6f7
mahmudul626 <mdmahmudulhasan1511@gmail.com>
2024-12-09 00:42:13 +0600

    Add files via upload
    This program calculates your Body Mass Index (BMI) based on your weight and height. It helps categorize your BMI into underweight, normal weight, overweight, or obese.

---
+++ BMI Calculator.cxx
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/* I'm Md. Mahmudul Hasan Mabud.
+8 and 9th December 2024, at 12:28 am. i finished my simple practice project BMI calculator  */
+
+int main() {
+    
+    //input part
+    
+    int foot,inc,weight,hight;
+    float meter,sqr,bmi;
+    printf("write your hight s foot :");
+    scanf("%d",&foot);
+    printf("write your hight s inc :");
+    scanf("%d",&inc);
+    printf("write your weight :");
+    scanf("%d",&weight);
+    
+    //calculating part
+    
+    hight = foot * 12 + inc;
+    meter = hight * 0.0254;
+    sqr = meter * meter;
+    bmi = weight / sqr;
+    
+    printf("Your BMI is %.1f\n",bmi);
+    
+    // message part
+    
+    if(18.5<=bmi) {
+        if(24.9>=bmi) {
+            printf("normal");
+        }
+        else if (29.9>= bmi) {
+            printf("over weight");
+        }
+        else {
+        printf("obesity");
+        }
+    }
+    else {
+        printf("under weight");
+    }
+    
+    return 0;
+}
