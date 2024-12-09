98a0426574b0fbb192361cf1f49b59f36eb811b7
Md. Mahmudul Hasan Mabud <mdmahmudulhasan1511@gmail.com>
2024-12-09 08:51:27 +0600

    Update BMI Calculator.cxx

--- BMI Calculator.cxx
+++ BMI Calculator.cxx
@@ -6,25 +6,25 @@ int main() {
     
     //input part
     
-    int feet,inc,weight,hight;
+    int feet,inc,weight,height;
     float meter,sqr,bmi;
-    printf("write your hight s feet :");
+    printf("write your height s feet :");
     scanf("%d",&feet);
-    printf("write your hight s inc :");
+    printf("write your height s inc :");
     scanf("%d",&inc);
     printf("write your weight :");
     scanf("%d",&weight);
     
     //calculating part
     
-    hight = feet * 12 + inc;
-    meter = hight * 0.0254;
+    height = feet * 12 + inc;
+    meter = height * 0.0254;
     sqr = meter * meter;
     bmi = weight / sqr;
     
     printf("Your BMI is %.1f\n",bmi);
     
-    // message part
+    // BMI Category Part
     
     if(18.5<=bmi) {
         if(24.9>=bmi) {
